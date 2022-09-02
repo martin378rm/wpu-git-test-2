8fd61ce0fc72dacfc4cc5de107430845d4905924
Martin Rivaldo <martinrivaldo111@gmail.com>
2022-09-02 11:35:12 +0700

    menambahkan paragraf

---
+++ OneDrive/Desktop/wpu-git-test-2/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>git remote</title>
+</head>
+<body>
+    <h1>Git remote</h1>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quis reiciendis sed, blanditiis ipsam eaque eos. Exercitationem nulla corporis vero nisi perferendis totam consectetur, magnam laudantium numquam earum a quisquam porro.</p>
+</body>
+</html>
